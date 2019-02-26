290912d91464747a90c8720b6e45216e758f9e2c
Tim Wingfield <tim@timwingfield.com>
2019-02-25 21:24:28 -0500

    got storybook working

---
+++ stories/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Button } from '@storybook/react/demo';
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+  ));
+
+storiesOf('something else', module)
+  .add('with Arya', () => (
+    <Button>Arya rules</Button>
+  ));
